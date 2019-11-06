1a937bca809acc6c9efb9d00022fb268d9351ca7
JortvD <10116429+JortvD@users.noreply.github.com>
2019-11-06 12:33:48 +0100

    Add on exit hook to close the app on exit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const onExit = require("async-exit-hook");
 const Application = require("./src/app");
 const app = new Application();
 
-app.start()
-.then(() => console.log("Librify-backend has started"))
-.catch(e => {throw e})
+app.start();
+
+onExit(callback => {
+	app.stop().then(() => callback());
+});
+
+onExit.uncaughtExceptionHandler(err => {
+    app.logger.error(err);
+});
+
+onExit.unhandledRejectionHandler(err => {
+    app.logger.error(err);
+});
